e2daea46a781ab4d74bda02421ff134944e6bd1f
ekazheko <kazheko@xaker.ru>
2023-12-01 18:54:47 +0100

    Add delivery options provider

---
+++ Demo.TheRetailer.WebApp/Core/Delivery/CarrierGateways/AbcCarrierGateway.cs
@@ -0,0 +1,28 @@
+﻿using Demo.TheRetailer.WebApp.Models;
+
+namespace Demo.TheRetailer.WebApp.Core.Delivery.CarrierGateways
+{
+    public class AbcCarrierGateway : IProvideDeliveryOptions
+    {
+        public IEnumerable<DeliveryOption> Get(string originPostcode, string destinationPostcode, DateTime shippingDate)
+        {
+            return new[]
+            {
+                new DeliveryOption
+                {
+                    CarrierService = "Standard Delivery (ABC Carrier 2Day)",
+                    Cost = 4.99m,
+                    MaxTransitTime = 2,
+                    MinTransitTime = 3
+                },
+                new DeliveryOption
+                {
+                    CarrierService = "Expedited Delivery (ABC Carrier Next Day)",
+                    Cost = 6.99m,
+                    MaxTransitTime = 1,
+                    MinTransitTime = 1
+                }
+            };
+        }
+    }
+}
---
+++ Demo.TheRetailer.WebApp/Models/DeliveryOption.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo.TheRetailer.WebApp.Models
+{
+    public class DeliveryOption
+    {
+        public string CarrierService { get; set; }
+        public decimal Cost { get; set; }
+        public int MinTransitTime { get; set; }
+        public int MaxTransitTime { get; set; }
+    }
+}
