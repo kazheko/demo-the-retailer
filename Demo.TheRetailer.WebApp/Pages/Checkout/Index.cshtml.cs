ea0a3cc89b15b053460c50721da5a3ec6f994098
ekazheko <kazheko@xaker.ru>
2023-12-25 23:03:08 +0100

    add checkout page

---
+++ Demo.TheRetailer.WebApp/Pages/Checkout/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Demo.TheRetailer.WebApp.Pages.Checkout
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
